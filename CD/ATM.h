a48e0214ec32a1be575615bb9eda05a5b2666b0f
mazvira <mazvira@ukr.net>
2018-11-03 13:27:34 +0200

    Update ATM.h

--- CD/ATM.h
+++ CD/ATM.h
@@ -1,21 +1,28 @@
 #pragma once
 //create by Denys Baranov 02/11/18
+#include <iostream>
+using namespace std;
+#include <string>
 class MoneyCD;
 class ATM
 {
 public:
 	ATM();
-	ATM(Money&);
-	~ATM(void);
+	ATM(MoneyCD&);
+	~ATM();
+	bool IsCardValid();
+	void chooseLanguage();
+	bool checkPassword();
+	void chooseOperation();
+	void showBalance();
+	void withdrawMoney();
+	bool DoYouNeedCheque();
+	void chooseQuantityOfMoneyToWithdraw();
+	void additionalServices();
 
 private:
-	Money* _money;
+	unsigned int _idOfATM;
+	string _address;
+
+	MoneyCD* _money;
 };
-
-void a()
-{
-	int a = 1;
-	int& b = a;
-	int* c = &b;
-
-}
