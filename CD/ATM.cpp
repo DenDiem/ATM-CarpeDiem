378aa5095093f7155d4da25c4e72f671ebc2be60
mazvira <mazvira@ukr.net>
2018-11-03 18:19:03 +0200

    Update ATM.cpp

--- CD/ATM.cpp
+++ CD/ATM.cpp
@@ -19,10 +19,33 @@ ATM::~ATM(void)
 
 bool ATM::checkPassword(const CardCD& card, string password)
 {
-	return card->_password == password;
+	return card._password == password;
 }
 
 bool ATM::IsCardValid(const CardCD& card, string cardnumber)
 {
-	return card.cardnumber = cardnumber;
+	return card._cardnumber == cardnumber;
+}
+
+double ATM::showBalance(const CardCD& card)
+{
+	return card._amount;
+}
+
+void ATM::withdrawMoney(const CardCD& card, unsigned int amount)
+{
+	card._amount - amount;
+}
+
+void ATM::DoYouNeedCheque(bool check, const CardCD& card, unsigned int amount)
+{
+	if (check)
+		printCheque(card, amount);
+}
+
+void ATM::printCheque(const CardCD& card, unsigned int amount)
+{
+	cout << "You had: " << card._amount << endl;
+	cout << "You withdraw: " <<amount<< endl;
+	cout << "Now your amount:" << card._amount - amount<< endl;
 }
