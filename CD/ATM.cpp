dd4c9889f20835deaab388d135fc93fc327411ea
mazvira <mazvira@ukr.net>
2018-11-03 13:26:31 +0200

    Update ATM.cpp

--- CD/ATM.cpp
+++ CD/ATM.cpp
@@ -1,18 +1,28 @@
-#include "ATM.h"
+#pragma once
 //create by Denys Baranov 02/11/18
-
-ATM::ATM(void):
-_money(0)
+#include <iostream>
+using namespace std;
+#include <string>
+class MoneyCD;
+class ATM
 {
-}
+public:
+	ATM();
+	ATM(MoneyCD&);
+	~ATM();
+	bool IsCardValid();
+	void chooseLanguage();
+	bool checkPassword();
+	void chooseOperation();
+	void showBalance();
+	void withdrawMoney();
+	bool DoYouNeedCheque();
+	void chooseQuantityOfMoneyToWithdraw();
+	void additionalServices();
 
-ATM::ATM( Money& mn):
-_money(&(mn))
-{
-}
+private:
+	unsigned int _idOfATM;
+	string _address;
 
-
-ATM::~ATM(void)
-{
-	delete _money;
-}
+	MoneyCD* _money;
+};
