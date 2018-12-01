d0fd89ffc544a6b3fc1aa04e895c4e39b688c629
mazvira <mazvira@ukr.net>
2018-12-01 14:48:42 +0200

    Update carpediematm.h

--- CarpeDiem/carpediematm.h
+++ CarpeDiem/carpediematm.h
@@ -5,34 +5,33 @@
 class CarpeDiemATM
 {
     private:
-    unsigned _atmID;
+    unsigned int _atmID;
     QString _address;
     Amount _amount;
 
 
      public:
-        CarpeDiemATM(const QString &atmID="test"); //take date from db (SQL query) by _atmID and create class
+        CarpeDiemATM(const unsigned &atmID=0); //take date from db (SQL query) by _atmID and create class
         const CarpeDiemATM& operator=(const CarpeDiemATM&);
-          CarpeDiemATM(const CarpeDiemATM&);//copy
+        CarpeDiemATM(const CarpeDiemATM&);//copy
         ~CarpeDiemATM();
 
         //selector-modifier
         Amount& amount();
         QString& address();
-        unsigned& atmID(){return _atmID;}
+        unsigned int& atmID();
 
         //selector
         const Amount& amount() const;
         const QString& address() const;
-        const unsigned & atmID() const{return _atmID;}
+        const unsigned int& atmID() const;
 
         //functions
         void updateATM(); //update ATM, use mySql query
         void deleteATM(); //delete ATM, use mySql query
 
-
-
         static CarpeDiemATM currentATM;
 
 };
+
 #endif // CARPEDIEMATM_H
