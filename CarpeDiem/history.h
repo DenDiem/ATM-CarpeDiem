055b6ab05c4b60d79679a5e09bbb7d41c2b6f0cd
mazvira <mazvira@ukr.net>
2018-12-01 18:23:47 +0200

    Update history.h

--- CarpeDiem/history.h
+++ CarpeDiem/history.h
@@ -2,6 +2,9 @@
 #define HISTORY_H
 
 #include <QDialog>
+#include <QDateTime>
+#include "useratm.h"
+#include "carpediematm.h"
 
 namespace Ui {
 class History;
@@ -14,9 +17,24 @@ class History : public QDialog
 public:
     explicit History(QWidget *parent = 0);
     ~History();
+    History();
+
+    //selector
+
+    const QDateTime& date() const;
+    const QString& address() const;
+
+    QDateTime& date();
+    QString& address();
 
 private:
     Ui::History *ui;
+
+    UserATM _user;
+    QDateTime _date;
+    QString _address;
+    CarpeDiemATM _ATM;
+
 };
 
 #endif // HISTORY_H
