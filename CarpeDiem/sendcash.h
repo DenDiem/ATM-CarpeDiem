5a6fc28770f117f5df1d6e4c3ea53bd3552ab799
mazvira <mazvira@ukr.net>
2018-12-01 18:22:18 +0200

    Update sendcash.h

--- CarpeDiem/sendcash.h
+++ CarpeDiem/sendcash.h
@@ -2,6 +2,7 @@
 #define SENDCASH_H
 
 #include <QDialog>
+#include "useratm.h"
 
 namespace Ui {
 class sendCash;
@@ -14,9 +15,11 @@ class sendCash : public QDialog
 public:
     explicit sendCash(QWidget *parent = 0);
     ~sendCash();
+    void sendMoney(UserATM& sender, UserATM& reciever, double& amount);
 
 private:
     Ui::sendCash *ui;
+
 };
 
 #endif // SENDCASH_H
